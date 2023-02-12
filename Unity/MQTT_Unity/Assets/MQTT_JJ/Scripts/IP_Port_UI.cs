2a45338f5ab72d9ee633ea2a009dcf11216633db
jerome jossent <jerome.jossent@veolia.com>
2023-02-12 18:29:01 +0100

    IP fix using inutile et bloquant

--- Unity/MQTT_Unity/Assets/MQTT_JJ/Scripts/IP_Port_UI.cs
+++ Unity/MQTT_Unity/Assets/MQTT_JJ/Scripts/IP_Port_UI.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEditor.PlayerSettings;
 
 public class IP_Port_UI : MonoBehaviour
 {
